d5560090e6bccd0ec137c2ecea209ca9e662cd31
drebee <ebennett@kehillah.org>
2024-10-17 18:01:16 -0700

    implement sonars

--- challengebot.py
+++ challengebot.py
@@ -3,6 +3,9 @@ import os
 import numpy as np
 import sys
 
+import RPi.GPIO as GPIO
+import time
+
 frame = 0
 debug = False
 
@@ -105,17 +108,60 @@ class SimulatorDriver:
 
 # Real Robot Driver
 class RealRobotDriver:
-    def __init__(self, robot_hardware):
-        self.robot_hardware = robot_hardware
+    def __init__(self):
+        
+        #GPIO Mode (BOARD / BCM)
+        GPIO.setmode(GPIO.BCM)
+
+        #set GPIO Pins
+        self.GPIO_LEFT_TRIGGER = 5
+        self.GPIO_LEFT_ECHO = 6
+        self.GPIO_RIGHT_TRIGGER = 17
+        self.GPIO_RIGHT_ECHO = 27
+
+        #set GPIO direction (IN / OUT)
+        GPIO.setup(self.GPIO_LEFT_TRIGGER, GPIO.OUT)
+        GPIO.setup(self.GPIO_LEFT_ECHO, GPIO.IN)
+        GPIO.setup(self.GPIO_RIGHT_TRIGGER, GPIO.OUT)
+        GPIO.setup(self.GPIO_RIGHT_ECHO, GPIO.IN)
+
+    def sonars(self):
+        left_distance = self.sonar(self.GPIO_LEFT_TRIGGER, self.GPIO_LEFT_ECHO)
+        right_distance = self.sonar(self.GPIO_RIGHT_TRIGGER, self.GPIO_RIGHT_ECHO)
+        return left_distance, right_distance
+
+    def sonar(self, GPIO_TRIGGER, GPIO_ECHO):
+        # set Trigger to HIGH
+        GPIO.output(GPIO_TRIGGER, True)
+
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIGGER, False)
+
+        StartTime = time.time()
+        StopTime = time.time()
+
+        # save StartTime
+        while GPIO.input(GPIO_ECHO) == 0:
+            StartTime = time.time()
+
+        # save time of arrival
+        while GPIO.input(GPIO_ECHO) == 1:
+            StopTime = time.time()
+
+        # time difference between start and arrival
+        TimeElapsed = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34300) / 2
+
+        return distance
     
     def motors(self, left, right, seconds):
         # Call real robot hardware control for left motor
-        self.robot_hardware.set_left_motor_speed(left)
-        self.robot_hardware.set_right_motor_speed(right)
-    
-    def sonars(self):
-        # Get distance from real robot sensor
-        return self.robot_hardware.get_sonar_distance()
-    
+        #self.robot_hardware.set_left_motor_speed(left)
+        #self.robot_hardware.set_right_motor_speed(right)
+        pass
+
     def exit(self):
         return
