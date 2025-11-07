539fbd24ecf6eff1f1214f9d56416da94b9096f9
drebee <ebennett@kehillah.org>
2025-11-07 11:40:03 -0800

    starter code

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ from simulator import robot, FORWARD, BACKWARD, STOP
 # Use robot.motors() to move
 # Use robot.left_sonar() and robot.right_sonar() to sense obstacles
 
+left_distance_in_cm = robot.left_sonar()
+print(left_distance_in_cm)
+
+robot.motors(left = FORWARD, right = FORWARD, seconds = 20)
+
 # When you're done, close the simulator
 robot.exit()
